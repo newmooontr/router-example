f208691a49ff9df12e40e32dc2dd127122276f0b
newmoon <newmooon.tr@gmail.com>
2022-11-02 01:38:33 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Footer from "./components/Footer";
+import Nav from "./components/Nav";
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
+import People from "./pages/People";
+import NotFound from "./pages/NotFound";
+import {Routes, Route} from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Nav/>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/people' element={<People/>} />
+        <Route path='/paths' element={<Paths/>} />
+        <Route path='/contact' element={<Contact/>} />
+        <Route path='*' element={<NotFound/>} />
+      </Routes>
+
+      <Footer/>
+    
+    </>
+    
   );
 }
 
